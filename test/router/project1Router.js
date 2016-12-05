9f15f88c9d0ebe9dc227a8a00e4747943cd9b93d
Midday <598535774@qq.com>
2016-12-05 12:19:22 +0800

    修复测试用例Bug

--- test/router/project1Router.js
+++ test/router/project1Router.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var indexController = require('../controllers/project1/indexController.js');
+var indexController = require('../controller/project1/indexController.js');
 
 router.get("/project1/index", indexController.gotoIndex);
 
